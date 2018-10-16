e02a891bbefd2c77bd0e54887725dc0dbd67adc0
Upendra Sharma <upendra861@gmail.com>
2018-10-16 22:24:17 +0530

    Update custom.js

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -1,9 +1,9 @@
 
     var feed = new Instafeed({
         get: 'user',
-        userId: '1500802915',
-        clientId: '250d10fbfe054e14ac868426179b2f7b',
-        accessToken:'1500802915.1677ed0.b9a04e0ff89b42e7976f339beaf42b34',
+        userId: '3525383132',
+        clientId: '960d25385c4a48b4a59cb371638397b0',
+        accessToken:'3525383132.1677ed0.b1e12d230e17468980dcc5789516c356',
         resolution:'standard_resolution',
         sortBy:'most-liked',
         template: '<div class="col-sm-6 col-md-6 col-lg-4 p-3" style="padding-bottom:15px;"><a href="{{image}}"><div class="imgcontainer"><img src="{{image}}" /></div></a></div>'
